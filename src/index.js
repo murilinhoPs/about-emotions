65899089ba7e30165fe2c013baea97382a0d7c9f
MuriloPs <murilinhops@pop-os.localdomain>
2020-06-12 15:58:14 -0300

    ftp

--- src/index.js
+++ src/index.js
@@ -13,4 +13,8 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-
-serviceWorker.unregister();
+serviceWorker.register();
+//ftp://murilo.pescatori%2540irislab.com.br@
+//ftp.irislab.com.br/murilo/buildEmotions/static/js/2.b5ad1ced.chunk.js
+//static/js/main.db8a60dd.chunk.js
+//static/js/runtime-main.0bccc4c2.js
